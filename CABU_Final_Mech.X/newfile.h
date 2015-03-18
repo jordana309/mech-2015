41d003b79f1b262db4f7606ed5894ee6164082c3
Trevor Buckner <trevor.buckner@byu.net>
2015-03-18 11:18:43 -0600

    Forgot to add the test header file.

---
+++ CABU_Final_Mech.X/newfile.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   newfile.h
+ * Author: Trevor
+ *
+ * Created on March 17, 2015, 9:16 PM
+ */
+
+#ifndef NEWFILE_H
+#define	NEWFILE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* NEWFILE_H */
+
